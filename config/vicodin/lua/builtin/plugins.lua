f90489637405196b329d1479ad509798b311d5f3
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2025-11-23 20:58:49 +0100

    Fix treesitter config

--- config/vicodin/lua/builtin/plugins.lua
+++ config/vicodin/lua/builtin/plugins.lua
@@ -81,7 +81,7 @@ vim.lsp.enable({
 })
 
 -- TreeSitter
-require('nvim-treesitter').setup({
+require('nvim-treesitter.configs').setup({
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
